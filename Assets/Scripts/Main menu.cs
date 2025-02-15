a2b6e8c3e437c1e0cfd295ab24a179c94d8cef0a
Deathnoisee <adlenalger2005@gmail.com>
2025-02-15 21:33:01 +0100

    new main menu with script

---
+++ Assets/Scripts/Main menu.cs
@@ -0,0 +1,29 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startgame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public void Startgame()
+    {
+        SceneManager.LoadScene("level 1");
+    }
+    
+    
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
