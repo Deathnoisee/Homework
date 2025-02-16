853ae01d258865beda78006121ddc63a8b6cb545
Deathnoisee <adlenalger2005@gmail.com>
2025-02-16 19:50:05 +0100

    trigger on chest to open new ground

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    public GameObject mapObject; // Assign the object you want to change in the map
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Make sure the player has the tag "Player"
+        {
+            Debug.Log("Player collided! Changing the map...");
+            mapObject.SetActive(!mapObject.activeSelf); // Toggle visibility
+        }
+    }
+}
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -56,6 +56,9 @@ public class movement : MonoBehaviour
                 isGrounded = false;
             }
         }
-    }
+   
+
+}
+
 
 
